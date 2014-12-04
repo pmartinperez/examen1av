d3be5349c483bedafc069881016f918333bfece2
pmartinperez <pmartinperez@eniac24.danielcastelao.org>
2014-12-04 10:05:23 +0100

    Commit final de ejercicio. Realizado de refactorizacion y revert al primer commit.

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,4 +1,3 @@
-
 package examencod;
 
 public class ExamenCOD {
@@ -7,26 +6,35 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
-System.out.println("Es numero primo");
-else
- System.out.println("NO es numero primo");           	 
-          	 
-for (int x=2;x<1000;x++) {
-if (metodoMio(x))
-System.out.print(x + " ");                         	 
-}         	 
-	}
-public static boolean metodoMio(int variable){
-int j = 2;
-boolean aux=true;
-while ((aux) && (j!=variable)){
-if (variable % j == 0)
-aux = false;
-j++;
-}
-return aux;
-  } 	 
+        int iN = 11;
+        if (metodoMio(iN)) {
+            System.out.println("Es numero primo");
+        } else {
+            System.out.println("NO es numero primo");
+        }
+
+        for (int x = 2; x < 1000; x++) {
+            if (metodoMio(x)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+
     
+    //1.Para variable=16 y j=2 la variable aux retorna con el valor true
+    //2.Para variable=22 y j=21 la expresion es true
+    
+    
+    public static boolean metodoMio(int variable) {
+        int j = 2;
+        boolean aux = true;
+        while ((aux) && (j != variable)) {
+            if (variable % j == 0) {
+                aux = false;
+            }
+            j++;
+        }
+        return aux;
+    }
+
 }
